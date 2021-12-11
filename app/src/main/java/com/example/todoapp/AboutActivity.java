73534eae973970ddb132b1882aaec47e0f96827b
mine rala <mine-rala@hotmail.com>
2021-12-11 13:51:09 +0300

    About Screen added

--- app/src/main/java/com/example/todoapp/AboutActivity.java
+++ app/src/main/java/com/example/todoapp/AboutActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,8 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        textView = findViewById(R.id.textViewAbout);
 
+        textView.setText(R.string.about_text);
     }
 }
